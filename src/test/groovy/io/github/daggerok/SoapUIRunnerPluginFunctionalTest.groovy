c907380b4664eab4fe6de3d9b830dac43943dcb9
Maksim Kostromin <daggerok@gmail.com>
2017-07-23 23:24:54 +0300

    Add test.

--- src/test/groovy/io/github/daggerok/SoapUIRunnerPluginFunctionalTest.groovy
+++ src/test/groovy/io/github/daggerok/SoapUIRunnerPluginFunctionalTest.groovy
@@ -37,7 +37,7 @@ import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
 class SoapUIRunnerPluginFunctionalTest extends Specification {
 
-  static final head = getGradleBuildHead('5.1.2-hotfix.2')
+  static final head = getGradleBuildHead('5.1.3')
 
   @Rule final TemporaryFolder testProjectDir = new TemporaryFolder()
 
